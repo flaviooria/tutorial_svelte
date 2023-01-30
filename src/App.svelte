7d53dd91aecdccd5b65d732d450b41fbd50399d8
Flavio Oria <flavio.oriap@gmail.com>
2023-01-30 11:35:03 +0100

    Usando la etiqueta @html

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,9 @@
   // Importando nuestro primer componente
   // Los componentes en svelte no requieren un export para usarlos, solo se importan
   import Atributes from './lib/components/Atributes.svelte';
+  import TagHtml from './lib/components/TagHtml.svelte';
 </script>
 
 <!--  Usando nuestro componente -->
-<Atributes />
+<!-- <Atributes /> -->
+<TagHtml />
---
+++ src/lib/components/TagHtml.svelte
@@ -0,0 +1,10 @@
+<script>
+    // Vamos a aprender a usar la tag @html, esto nos servira por si en alguna variable tenga un etiqueta html
+    // esta la pueda interpretar como tal.
+    let tagHtml = 'Hola developers, haremos uso de la etiqueta <b><code>{@html attribute}</code></b>'
+</script>
+
+<p>Esto es sin usar la etiqueta html</p>
+<p>{tagHtml}</p>
+<p>Usando la etiqueta</p>
+<p>{@html tagHtml}</p>
