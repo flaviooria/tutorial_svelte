fce5a3ea9a57f48ed4bf6fc4f9f258bec54565ed
Flavio Oria <flavio.oriap@gmail.com>
2023-01-30 11:51:12 +0100

    Reactividad - Asignación reactiva

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,10 @@
   // Importando nuestro primer componente
   // Los componentes en svelte no requieren un export para usarlos, solo se importan
   import Atributes from './lib/components/Atributes.svelte';
+  import Assignaments from './lib/components/Assignaments.svelte';
 </script>
 
 <!--  Usando nuestro componente -->
 <Atributes />
+<br>
+<Assignaments></Assignaments>
---
+++ src/lib/components/Assignaments.svelte
@@ -0,0 +1,22 @@
+<script>
+    // Trabajaremos la reactividad, donde tocaremos 4 puntos 
+    /**
+     * Asignación reactiva
+     * Declarión reactiva
+     * Manejo de estado
+     * Actualizando arrays y objetos
+     */
+
+    let count = 0;
+
+    function incrementCount() {
+		// Manejaremos la reactividad que tiene svelte donde manipula directamente el dom, con nuestra variable declarada previamente
+        // Esto se denomina asignación reactiva, ya que cada vez que hagamos click se activara la función y sumara +1 al count
+        count++;
+	}
+</script>
+
+<!-- Usaremos el evento on:click para hacer uso de nuestra función definida -->
+<button on:click={incrementCount}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
