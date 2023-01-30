415544293e2d40181fb03c6da6f792dfc1b4e918
Flavio Oria <flavio.oriap@gmail.com>
2023-01-30 17:51:46 +0100

    Bindings - bind tipo value

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,9 @@
   // Importando nuestro primer componente
   // Los componentes en svelte no requieren un export para usarlos, solo se importan
   import Atributes from './lib/components/Atributes.svelte';
+  import Input from './lib/components/Input.svelte';
 </script>
 
 <!--  Usando nuestro componente -->
-<Atributes />
+<!-- <Atributes /> -->
+<Input />
---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,16 @@
+<script>
+    // Vamos a trabajar con los bindings, el uso que tienen es que nos facilitarán el acceso a un dato que queramos obtener. Por ejemplo si en input, normalmente para obtener el valor lo tenemos que coger con un event.target.value
+    // Con los bind nos simplifica mucho esa tarea obteniendo el valor del input y actualizando a la vez la  variable
+    let name = ''
+
+    $: if (name) {
+        console.log(name);
+    }
+</script>
+
+<!-- Sin bind -->
+<input type="text" value={name} placeholder="Escribe algo ...">
+<p>Sin bind: {name}</p>
+<!-- Con bind -->
+<input type="text" bind:value={name} placeholder="Escribe algo ...">
+<p>Con bind: {name}</p>
