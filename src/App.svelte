ea5396e84ef0ef766f1e307415461b8d899523ed
Flavio Oria <flavio.oriap@gmail.com>
2023-01-30 12:17:43 +0100

    Reactividad - Actualizando arrays y objetos

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
   // Los componentes en svelte no requieren un export para usarlos, solo se importan
   import Atributes from './lib/components/Atributes.svelte';
   import Assignaments from './lib/components/Assignaments.svelte';
+  import UpdateArrays from './lib/components/UpdateArrays.svelte';
 </script>
 
 <!--  Usando nuestro componente -->
 <!-- <Atributes /> -->
 <br>
-<Assignaments></Assignaments>
+<!-- <Assignaments></Assignaments> -->
+<UpdateArrays></UpdateArrays>
---
+++ src/lib/components/UpdateArrays.svelte
@@ -0,0 +1,16 @@
+<script>
+  // Veremos como actulizar el array y objetos
+  let numbers = [1, 2, 3, 4, 5];
+
+  const addNumber = () => {
+    numbers = [...numbers, numbers.length + 1];
+  };
+
+  $: joinedNumbers = numbers.join(' + ')
+  $: totalNumbers = numbers.reduce((a, b) => a + b);
+</script>
+
+<button on:click={addNumber}>
+    Agregar número
+</button>
+<p>{joinedNumbers} = {totalNumbers}</p>
