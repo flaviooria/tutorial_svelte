579ee136a619a0f62368cf335247002f7c02197d
Flavio Oria <flavio.oriap@gmail.com>
2023-01-30 12:29:05 +0100

    Props - Pasando props entre padres a hijos

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,11 @@
   // Importando nuestro primer componente
   // Los componentes en svelte no requieren un export para usarlos, solo se importan
   import Atributes from './lib/components/Atributes.svelte';
+  import Props from './lib/components/Props.svelte';
 </script>
 
 <!--  Usando nuestro componente -->
-<Atributes />
+<!-- <Atributes /> -->
+
+<!-- Pasando props a nuestro componente hijo -->
+<Props keyword={'<b>keyword</b>'}></Props>
---
+++ src/lib/components/Props.svelte
@@ -0,0 +1,7 @@
+<script>
+    // Vamos a obtener el uso de props, usando la palabra clave "export"
+    export let keyword
+
+</script>
+
+<p>Esta es un props pasada por un componente padre: {@html keyword}</p>
